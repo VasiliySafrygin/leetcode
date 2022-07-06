7318ac54311680c2d0e71fc32a37b39ff1b42163
Vasiliy Safrygin <vsafrygin@yandex.ru>
2022-07-06 21:36:15 +0300

    Сделал наименование файлов однотипным

---
+++ 191. Number of 1 Bits.py
@@ -0,0 +1,31 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        while(n != 0):
+            n = n & (n - 1)
+            cnt += 1
+        return cnt
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    r = s.hammingWeight(0b00000000000000000000000000001011)
+    assert (r == 3)
+    print(r)
+
+    r = s.hammingWeight(0b00000000000000000000000010000000)
+    assert (r == 1)
+    print(r)
+
+    r = s.hammingWeight(0b11111111111111111111111111111101)
+    assert (r == 31)
+    print(r)
+
+    r = s.hammingWeight(0b00000000000000000000000000000001)
+    assert (r == 1)
+    print(r)
+
+    r = s.hammingWeight(0b00000000000000000000000000000000)
+    assert (r == 0)
+    print(r)
